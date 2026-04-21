35429cc7a86b8130d80456b516ea38c409986eb9
Fedora-Notebook-Michael <michael.moura72@hotmail.com>
2026-04-20 21:56:01 -0300

    ✨ Status update implemented to "in transit".

--- src/main/java/com/michael/global_logistics_api/logistics/domain/Controller/ShipmentController.java
+++ src/main/java/com/michael/global_logistics_api/logistics/domain/Controller/ShipmentController.java
@@ -1,22 +1,46 @@
 package com.michael.global_logistics_api.logistics.domain.Controller;
 
-
 import com.michael.global_logistics_api.logistics.domain.model.Shipment;
+import com.michael.global_logistics_api.logistics.domain.model.ShipmentStatus;
 import com.michael.global_logistics_api.logistics.domain.service.ShipmentService;
+import com.michael.global_logistics_api.logistics.domain.repository.ShipmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/v1/shipments")
 @RequiredArgsConstructor
 public class ShipmentController {
 
-    private final ShipmentService ShipmentService;
+    private final ShipmentService shipmentService;
+    private final ShipmentRepository shipmentRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Shipment create(@RequestBody Shipment shipment) {
-        return ShipmentService.createShipment(shipment);
+        return shipmentService.createShipment(shipment);
     }
+
+    @GetMapping
+    public List<Shipment> list() {
+        return shipmentRepository.findAll();
+    }
+
+    @PatchMapping("/{id}/status")
+    public Shipment updateStatus(@PathVariable UUID id, @RequestParam ShipmentStatus newStatus) {
+        Shipment shipment = shipmentRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        shipment.setStatus(newStatus);
+        return shipmentRepository.save(shipment);
+    }
+
 }
--- src/main/java/com/michael/global_logistics_api/logistics/domain/service/ShipmentService.java
+++ src/main/java/com/michael/global_logistics_api/logistics/domain/service/ShipmentService.java
@@ -1,10 +1,14 @@
 package com.michael.global_logistics_api.logistics.domain.service;
 
 import com.michael.global_logistics_api.logistics.domain.model.Shipment;
+import com.michael.global_logistics_api.logistics.domain.model.ShipmentStatus;
 import com.michael.global_logistics_api.logistics.domain.repository.ShipmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.UUID;
 
 
@@ -16,13 +20,22 @@ public class ShipmentService {
 
     @Transactional
     public Shipment createShipment(Shipment shipment) {
-        // Regra de négocio: Gerar um tracking number único para cada envio
-        if(shipment.getTrackingNumber() == null) {
-            shipment.setTrackingNumber("TRK--" + UUID.randomUUID().toString().substring(0, 8).toUpperCase());
+        // Validação de segurança
+        if(shipment.getInsuranceValue().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("O valor do seguro deve ser maior que zero");
         }
+        // Geração automática do tracking (Se não vier preenchido)
+        if (shipment.getTrackingNumber() == null || shipment.getTrackingNumber().isEmpty()) {
+            String prefix = "GLA-" + LocalDate.now().getYear() + "-";
+            String uniqueId = UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+            shipment.setTrackingNumber(prefix + uniqueId);
+        }
+
+        // Garante o status incial
+        // (Isso já pode estar no @PrePersist, mas garantir aqui é boa prática)
+        shipment.setStatus(ShipmentStatus.PENDING);
+        
         return shipmentRepository.save(shipment);
     }    
-
-
 
 }
